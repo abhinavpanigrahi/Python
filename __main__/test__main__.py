2cc29f5012af501d214f68c83e8d5a1634f2000b
macbookpro-abh <abhinavpanigrahi.project@gmail.com>
2025-10-25 23:07:30 +0530

    test __name__ == '__main__'

---
+++ __main__/test__main__.py
@@ -0,0 +1,17 @@
+
+print("starting code")
+
+def func1():
+    print("func1")
+def func2():
+    pass
+def func3():
+    print("func3")
+
+if __name__ == '__main__':
+    func1()
+    func2()
+    func3()
+    print("__init__ executed")
+
+print("ending code")
